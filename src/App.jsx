93f59a0cc6b0785350252c4db8be283275be681d
Xavier Georget <xageorget@gmail.com>
2023-06-30 17:38:40 +0200

    editor tabs, logo and full logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Home from "./pages/Home";
 function App() {
   return (
     <main className="app transition-all ease-in">
-      <Canvas />
       <Customizer />
       <Home />
+      <Canvas />
     </main>
   );
 }
